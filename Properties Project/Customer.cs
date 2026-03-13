ca6d4102dbc7829e6f52a98d3f348c7e33d7371d
Durga Prasanna <prasannadurga689@gmail.com>
2026-03-13 19:32:27 +0530

    Add project files.

---
+++ Properties Project/Customer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_Project
+{
+        public class Customer
+        {
+            int _CustID;
+            bool _Status;
+            string _Cname;
+            double _Balance;
+            public Customer(int CustID, bool Status, string Cname, double Balance)
+            {
+                _CustID = CustID;
+                _Status = Status;
+                _Cname = Cname;
+                _Balance = Balance;
+            }
+            public int CustID
+            {
+                get { return _CustID; }
+                set { _CustID = value; }
+            }
+            public bool Status
+            {
+                get { return _Status; }
+                set { _Status = value; }
+            }
+            public string Cname
+            {
+                get
+                {
+                    return _Cname;
+                }
+                set
+                {
+                    if (_Status == true)
+                        _Cname = value;
+                }
+            }
+            public double Balance
+            {
+                get
+                {
+                    return _Balance;
+                }
+                set
+                {
+                    if (_Status == true)
+                    {
+                        if (value >= 500)
+
+
+                        {
+                            _Balance = value;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    internal class Customer
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
---
+++ Properties Project/TestCustomer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_Project
+{
+        class TestCustomer
+        {
+            static void Main()
+            {
+                Customer obj = new Customer(101, false, "John", 5000.0);
+                Console.WriteLine("Customer ID " + obj.CustID);
+
+                //obj.CustID = 102; // Cant assigned as the property is read only if we dont set the value
+                obj.CustID = 102; //here we set the value
+                Console.WriteLine("Modified Customer ID : " + obj.CustID);
+                if (obj.Status == true)
+                {
+                    Console.WriteLine("Current customer status : active");
+                }
+                else
+                {
+                    Console.WriteLine("Current customer status : In-Active");
+                }
+                Console.WriteLine("Customer Name : " + obj.Cname);
+                obj.Cname += " Smith";
+                Console.WriteLine("Modified Customer Name : " + obj.Cname); //assigned failed . so below smith wont be added to the name as the status is false
+                Console.WriteLine("Customer Balance : " + obj.Balance);
+                obj.Balance -= 3000.0;
+                Console.WriteLine("Modified Customer Balance : " + obj.Balance); //assigned failed . so below 1000 wont be deducted from the balance as the status is false
+
+                obj.Status = true; // here we set the status to true so that we can modify the name and balance
+                Console.WriteLine("----------");
+                if (obj.Status == true)
+                {
+                    Console.WriteLine("Current customer status : active");
+                }
+                else
+                {
+                    Console.WriteLine("Current customer status : In-Active");
+                }
+
+                obj.Cname += " Smith";
+                Console.WriteLine("Modified Customer Name : " + obj.Cname); //assigned success . so below smith will be added to the name as the status is true
+
+
+                obj.Balance -= 3000.0;
+                Console.WriteLine("Modified Customer Balance : " + obj.Balance); //assigned success . so below 1000 will be deducted from the balance as the status is true
+                Console.ReadLine();
+                obj.Balance = obj.Balance - 1600; //Assignment fails, so prints old balance
+                Console.WriteLine(" balance when assignment fails : " + obj.Balance);
+
+                obj.Balance = obj.Balance - 1400; //Assignment Succeeds, so prints new balance
+                Console.WriteLine(" balance when assignment succeeds : " + obj.Balance);
+            }
+        }
+    }
+
+    internal class TestCustomer
+    {
+    }
+
